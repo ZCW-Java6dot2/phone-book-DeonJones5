f0d25c03cd3f1dbab41cc3991964db900d2a83a3
deon <deon@zipcodes-MacBook-Pro-23.local>
2020-11-12 16:38:11 -0500

    Finished PhoneBook

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,39 +13,73 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        phonebook = new LinkedHashMap<String, List<String>>();
+
     }
 
     public void add(String name, String phoneNumber) {
+        if (phonebook.containsKey(name)){
+            List<String> updatedNumbers = phonebook.get(name);
+            updatedNumbers.add(phoneNumber);
+            phonebook.replace(name, updatedNumbers);
+        } else {
+            phonebook.put(name, Collections.singletonList(phoneNumber));
+        }
+
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        if (phonebook.containsKey(name)){
+            List<String> updatedNumbers = phonebook.get(name);
+            updatedNumbers.addAll(Arrays.asList(phoneNumbers)); // coverting array to list
+            phonebook.replace(name, updatedNumbers);
+        } else {
+            phonebook.put(name, Arrays.asList(phoneNumbers));
+        }
     }
 
     public void remove(String name) {
+        phonebook.remove(name);
+
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return phonebook.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+
+
+        return phonebook.get(name);
     }
 
+    // look up by phone number and not name
     public String reverseLookup(String phoneNumber)  {
-        return null;
+       Set<String> allKeys = phonebook.keySet(); // list that have all unique values (Example - deck of cards have each set)
+        for (String key : allKeys){
+            List<String> values = phonebook.get(key);
+            if (values.contains(phoneNumber)){
+                return key;
+            }
+        } return "";
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> contactNames = new ArrayList<String>();
+        for (String key : phonebook.keySet()){
+            contactNames.add(key);
+
+        }
+        return contactNames;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
